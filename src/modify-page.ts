dbeb31dd2ac98bc8cf416cba13384b507ea87b4c
kminic-school <112583675+101010101010101011010@users.noreply.github.com>
2025-01-08 12:46:04 -0500

    Partial commit, added part of parameters

--- src/modify-page.ts
+++ src/modify-page.ts
@@ -1,4 +1,5 @@
-
+const START_PERCENT = 0.0;
+const END_PERCENT = 0.5;
 
 function isLetter(char: string) {
   return char.toLowerCase() != char.toUpperCase()
@@ -24,14 +25,42 @@ function modify(node: ChildNode) {
 
 
   for (var i=1; i < indices.length; i++) {
-    const boldText = text.substring(indices[i-1], (indices[i] + indices[i-1]) / 2);
+    const iDiff = indices[i] - indices[i-1];
+
+    if (START_PERCENT > END_PERCENT) { console.error("END_PERCENT must be smaller than START_PERCENT! This is an error in the implementation in code; if you are a user, please report this as a bug."); }
+
+
+    // Indices of key parts of the text
+    const textPartIndices = [
+      Math.floor(indices[i-1]), // Start of first plain-text
+      Math.floor(indices[i-1] + iDiff*START_PERCENT), // End of first plain-text; Start of bold
+      Math.floor(indices[i-1] + iDiff*END_PERCENT), // End of bold; Start of last plain-text
+      Math.ceil(indices[i]) // End of last plain-text
+    ]
+
+    // Add start plain-text component
+
+    var startPlainText = text.substring(textPartIndices[0], textPartIndices[1]); // Select the first plain subsection of the text
+    if (startPlainText.length > 0) {
+      const startTextNode = document.createTextNode(startPlainText);
+      parentSpan.appendChild(startTextNode);
+    }
+
+
+    // Create bold part of text
+    
+    const boldText = text.substring(textPartIndices[1], textPartIndices[2]);
     if (boldText.length > 0) {
       const boldNode = document.createElement("b");
-      boldNode.textContent = text.substring(indices[i-1], (indices[i] + indices[i-1]) / 2);
+      boldNode.textContent = boldText;
       parentSpan.appendChild(boldNode);
     }
 
+    //
     var newText = text.substring((indices[i] + indices[i-1]) / 2, indices[i]);
+
+
+
     while (i < indices.length && indices[i + 1] - indices[i] <= 1) {
       i++;
       newText += text.substring((indices[i] + indices[i-1]) / 2, indices[i]);
